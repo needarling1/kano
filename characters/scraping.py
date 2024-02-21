ea60814849f21b7c4e57d05aed0d7397db72b05e
needarling1 <97009120+needarling1@users.noreply.github.com>
2024-02-20 16:11:16 -0800

    developing scraping script for characters

--- characters/scraping.py
+++ characters/scraping.py
@@ -1,2 +1,19 @@
 from bs4 import BeautifulSoup
+import requests
+from selenium import webdriver
 
+ani_html = requests.get('https://anilist.co/search/characters/favorites').text
+bsoup = BeautifulSoup(ani_html, 'lxml')
+
+content = bsoup.find('div', class_ = 'page-content')
+search = content.find('div', class_ = 'search other-type')
+container = search.find('div', class_ = 'container')
+cover = container.find('div', class_ = 'results cover')
+
+character = cover.find('a')
+
+name = character.find('div', class_ = 'name').text
+img_link = character.find('div', class_ = 'cover').find('img')['src'].replace(' ', '')
+
+
+print(name, img_link)
